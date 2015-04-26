90f9bffbb1b3de17ec4f64742f36d8fe05b9e53a
Hopesa Camaraski (Hardin Zuhdi) <havespolo@gmail.com>
2015-04-26 19:14:08 +0700

    Update master.php

--- master.php
+++ master.php
@@ -9,7 +9,27 @@ $query = mysql_query($sql) or trigger_error("Query Failed: " . mysql_error());
 $dataid = mysql_fetch_assoc($query);
 $level = $dataid["rank"];
 $id_user = $dataid["id_user"];//ID User grabbed
-if($level!=1000){echo "403 Not Authorized Please Exit this Page"; exit;}?>
+if($level!=1000){echo "403 Not Authorized Please Exit this Page"; exit;}
+//Taking data
+$sql = mysql_query("SELECT * FROM `cart_final` where status='Pending Verifikasi'") or trigger_error("Query Failed: " . mysql_error());
+$newtranspenver=mysql_num_rows($sql);
+
+$sql = mysql_query("SELECT * FROM `cart_final` where status='Pending Konfirmasi'") or trigger_error("Query Failed: " . mysql_error());
+$penkon=mysql_num_rows($sql);
+
+$sql = mysql_query("SELECT * FROM `cart_final` where status='Seller Pending'") or trigger_error("Query Failed: " . mysql_error());
+$selpentrans=mysql_num_rows($sql);
+
+$sql = mysql_query("SELECT * FROM `cart_final` where status='Pending Verifikasi Resi'") or trigger_error("Query Failed: " . mysql_error());
+$penveres=mysql_num_rows($sql);
+
+$sql = mysql_query("SELECT * FROM `cart_final` where status='Pending Delivery'") or trigger_error("Query Failed: " . mysql_error());
+$deliverpen=mysql_num_rows($sql);
+
+$sql = mysql_query("SELECT * FROM `cart_final` where status='Selesai'") or trigger_error("Query Failed: " . mysql_error());
+$sls=mysql_num_rows($sql);
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -50,11 +70,11 @@ if($level!=1000){echo "403 Not Authorized Please Exit this Page"; exit;}?>
 				<div class="col-sm-6">
 					<div class="total_area">
 						<ul>
-                            <li>Transaksi Baru</li>
-							<li>Konfirmasi Transaksi </li>
-							<li>Transaksi Pending Verifikasi </li>
-							<li>Transaski Seller Pending</li>
-                            <li>Transaksi Selesai </li>
+                            <li>Transaksi Baru Hari Ini<span><?php echo $newtranspenver ?></span></li>
+							<li>Konfirmasi Transaksi <span><?php echo $penkon ?></span> </li>
+							<li>Transaksi Pending Verifikasi <span><?php echo $newtranspenver ?></span> </li>
+							<li>Transaski Seller Pending <span><?php echo $selpentrans ?></span> </li>
+                            <li>Transaksi Selesai <span><?php echo $sls ?></span> </li>
 						</ul>
 							<center><a class="btn btn-default check_out" href="">Lihat Master Panel Transaksi</a></center>
 					</div>
@@ -62,11 +82,11 @@ if($level!=1000){echo "403 Not Authorized Please Exit this Page"; exit;}?>
                 <div class="col-sm-6">
 					<div class="total_area">
 						<ul>
-							<li>Konfirmasi baru</li>
+							<li>Konfirmasi baru <span><?php echo $penkon ?></span> </li>
 							<li>Konfirmasi Deposit Dompetin</li>
 							<li>Konfirmasi Pembayaran Lain</li>
-                            <li>Konfirmasi Resi</li>
-                            <li>Verifikasi Delivery</li>
+                            <li>Konfirmasi Resi <span><?php echo $penveres ?></span></li>
+                            <li>Verifikasi Delivery<span><?php echo $deliverpen?></span></li>
                             <center><a class="btn btn-default check_out" href="">Lihat Panel Konfirmasi</a></center>
 						</ul>
 					</div>
@@ -87,7 +107,7 @@ if($level!=1000){echo "403 Not Authorized Please Exit this Page"; exit;}?>
                 <div class="col-sm-6">
 					<div class="total_area">
 						<ul>
-							<li>Staff Verifikasi Online:</li>
+							<li>Expected Expenses</li>
 							<li>Expected Profit Today</li>
 							<li>Turn Over</li>
                             <li>Uang Masuk Hari Ini</li>
@@ -97,7 +117,18 @@ if($level!=1000){echo "403 Not Authorized Please Exit this Page"; exit;}?>
 					</div>
                     
 				</div>
-                <center><div class="col-sm-12"><h4>Panel Lainnya</h4><button class="btn btn-default check_out">Cek Konfirmasi Pembayaran</button><button onclick="print_d()" class="btn btn-default check_out">Lihat Transaksi Baru</button><button class="btn btn-default check_out">Ke Dompetin Master Panel</button></div></center>
+                 <div class="col-sm-6 col-md-offset-3">
+					<div class="total_area">
+						<ul>
+							<li>Staff Verifikasi Online</li>
+							<li>Staff Service Online</li>
+							<li>Staff Checkers</li>
+                        <center> <a class="btn btn-default check_out" href="">Ke Staff Management Panel</a></center>
+						</ul>
+					</div>
+                    
+				</div>
+                <center><div class="col-sm-12"><h4>Panel Lainnya</h4><button class="btn btn-default check_out">Cek Konfirmasi Pembayaran</button><button onclick="print_d()" class="btn btn-default check_out">Lihat Transaksi Baru</button><button class="btn btn-default check_out">Ke Dompetin Master Panel</button><button class="btn btn-default check_out">Ke Database Management</button><button class="btn btn-default check_out">Github Repository</button></div></center>
 			</div>
 		</div><br><br><br><br>
 	</section><!--/#do_action-->
